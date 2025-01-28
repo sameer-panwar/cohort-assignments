0768fb52d4f74789d9d721b831fdcde226dd8548
sameer-panwar <sameerpanwar9720@gmail.com>
2025-01-28 11:07:32 +0530

    learned how a HTTP server is built

---
+++ HTTP server/index.js
@@ -0,0 +1,15 @@
+
+
+const express = require('express')
+const app = express()
+const port = 1000
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+  res.send('<b>Hello bhai log!!!!</b>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
